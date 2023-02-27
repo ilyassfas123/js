7ec124ade2be3fbec8fd992b066028851f6b01ba
ilyassfas123 <ilyassfaskaa123@gmail.com>
2023-02-27 02:02:14 +0100

    hi

---
+++ index.js
@@ -0,0 +1,13 @@
+var numb = prompt("entrez un nombre")
+while(numb <10 || numb >20 ){
+
+
+  if( Number(numb) >10) console.log("plus petit!")
+  else if(Number(numb) >20) console.log("plus grand!")
+  numb = prompt("Entrer un nombre entre 10 et 20")
+
+  
+}
+numb = "le nombre est correct"
+
+
